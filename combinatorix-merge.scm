d4b934f6b702204dcc89d11e8d53af227bb51445
Oleksandr Novak <novak.as@i.ua>
2020-05-28 11:24:03 +0300

    iterate over all possible layer combinations

--- combinatorix-merge.scm
+++ combinatorix-merge.scm
@@ -7,26 +7,33 @@
 
   (define newImage (car (gimp-image-new imageWidth imageHeight RGB)))  
 
-  (let loop ((times (expt 2 layersCount)))
-    (gimp-image-insert-layer 
-      newImage
-      (head 
-        (gimp-layer-new 
-          newImage imageWidth imageHeight RGB-IMAGE (number->string times) 100 LAYER-MODE-NORMAL))
-      0
-      0)
+  (define (iterateAllCombinations fromValue toValue skipAmount currentSet currentName)
 
-    (if (= times 0)
-      ()
-      (loop (- times 1))
-    )
-  )
-
+    (let (
+        (newSet (append (list (+ fromValue skipAmount)) currentSet))
+        (newName (string-append currentName "-" (number->string (+ fromValue skipAmount))))
+      )    
       
+      (gimp-image-insert-layer 
+        newImage
+        (head 
+          (gimp-layer-new 
+            newImage imageWidth imageHeight RGB-IMAGE newName 100 LAYER-MODE-NORMAL))
+        0
+        0)      
+    
+      (when (<= (+ 1 skipAmount fromValue) toValue)
+        (iterateAllCombinations (+ 1 fromValue) toValue skipAmount newSet newName))
+    )
 
+    (when (<= (+ 1 skipAmount fromValue) toValue)        
+      (iterateAllCombinations fromValue toValue (+ 1 skipAmount) currentSet currentName))   
+    )
+  
+
+  (iterateAllCombinations 0 layersCount 0 '() "layer")
 
   (gimp-display-new newImage)
-  
 )
 
 (script-fu-register "script-fu-combinatorix-merge"
