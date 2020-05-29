d9be93c67f8aa91fa6a4289afdc21e81c9126121
Oleksandr Novak <novak.as@i.ua>
2020-05-29 12:01:26 +0300

    create a separate image for every possible group of layers

--- combinatorix-merge.scm
+++ combinatorix-merge.scm
@@ -6,34 +6,46 @@
   (define layersCount (car (gimp-image-get-layers originalImage)))
 
   (define newImage (car (gimp-image-new imageWidth imageHeight RGB)))  
+  
+  (define (for-each list func)
+    (let loop ((rest list))
+      (unless (null? rest)
+        (func (car rest))
+        (loop (cdr rest))
+      )
+    )
+  )
 
-  (define (iterateAllCombinations fromValue toValue skipAmount currentSet currentName)
+  (define (iterate-all-combinations fromValue toValue skipAmount currentSet currentName)
 
     (let (
         (newSet (append (list (+ fromValue skipAmount)) currentSet))
         (newName (string-append currentName "-" (number->string (+ fromValue skipAmount))))
       )    
       
-      (gimp-image-insert-layer 
-        newImage
-        (head 
-          (gimp-layer-new 
-            newImage imageWidth imageHeight RGB-IMAGE newName 100 LAYER-MODE-NORMAL))
-        0
-        0)      
-    
-      (when (<= (+ 1 skipAmount fromValue) toValue)
-        (iterateAllCombinations (+ 1 fromValue) toValue skipAmount newSet newName))
+        (let (
+          (newImage (car (gimp-image-new imageWidth imageHeight RGB)))
+        )
+          (gimp-display-new newImage) 
+
+          (for-each newSet (lambda (layerId)
+            (display layerId)
+            (gimp-image-insert-layer 
+              newImage (head (gimp-layer-new newImage imageWidth imageHeight RGB-IMAGE "" 100 LAYER-MODE-NORMAL))
+              0 0))
+          )                    
+        )
+
+      (when (<= (+ 1 skipAmount fromValue) toValue)  
+        (iterate-all-combinations (+ 1 fromValue) toValue skipAmount newSet newName))  
     )
 
     (when (<= (+ 1 skipAmount fromValue) toValue)        
-      (iterateAllCombinations fromValue toValue (+ 1 skipAmount) currentSet currentName))   
-    )
+      (iterate-all-combinations fromValue toValue (+ 1 skipAmount) currentSet currentName))   
+  )
   
 
-  (iterateAllCombinations 0 layersCount 0 '() "layer")
-
-  (gimp-display-new newImage)
+  (iterate-all-combinations 1 layersCount 0 '() "layer")  
 )
 
 (script-fu-register "script-fu-combinatorix-merge"
