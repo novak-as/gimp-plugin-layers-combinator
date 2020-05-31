65123a4a3d00a3126d6eca46a2d69f50f86bdd58
Oleksandr Novak <novak.as@i.ua>
2020-05-31 12:22:26 +0300

    copy layers into generated images

--- combinatorix-merge.scm
+++ combinatorix-merge.scm
@@ -1,19 +1,22 @@
-(define (script-fu-combinatorix-merge)
-  (define imageWidth 50)
-  (define imageHeight 50)
-
-  (define originalImage (aref (cadr (gimp-image-list)) 0) )
-  (define layersCount (car (gimp-image-get-layers originalImage)))
-
-  (define newImage (car (gimp-image-new imageWidth imageHeight RGB)))  
-  
-  (define (for-each list func)
-    (let loop ((rest list))
-      (unless (null? rest)
-        (func (car rest))
-        (loop (cdr rest))
-      )
+(define (for-each list func)
+  (let loop ((rest list))
+    (unless (null? rest)
+      (func (car rest))
+      (loop (cdr rest))
     )
+  )
+)
+
+(define (script-fu-combinatorix-merge)
+  (let* (
+    (imageWidth 50)
+    (imageHeight 50)
+    (originalImage (aref (cadr (gimp-image-list)) 0))
+    (layersCount (car (gimp-image-get-layers originalImage)))
+  )
+
+  (define (get-layer-number image sequenceNumber)
+    (aref (cadr (gimp-image-get-layers image)) sequenceNumber)
   )
 
   (define (iterate-all-combinations fromValue toValue skipAmount currentSet currentName)
@@ -25,27 +28,32 @@
       
         (let (
           (newImage (car (gimp-image-new imageWidth imageHeight RGB)))
-        )
-          (gimp-display-new newImage) 
-
-          (for-each newSet (lambda (layerId)
-            (display layerId)
+        ) 
+          (for-each newSet (lambda (layerSequenceNumber)
+                        
             (gimp-image-insert-layer 
-              newImage (head (gimp-layer-new newImage imageWidth imageHeight RGB-IMAGE "" 100 LAYER-MODE-NORMAL))
-              0 0))
-          )                    
+              newImage
+              (head (gimp-layer-new newImage imageWidth imageHeight RGBA-IMAGE (number->string layerSequenceNumber) 100 LAYER-MODE-NORMAL))
+              0 0)
+
+            (gimp-image-set-active-layer originalImage (get-layer-number originalImage layerSequenceNumber))
+
+            (gimp-edit-copy (car (gimp-image-get-active-layer originalImage)))
+            (gimp-floating-sel-anchor (car (gimp-edit-paste (car (gimp-image-get-active-layer newImage)) TRUE)))
+          ))                    
+
+          (gimp-display-new newImage)
         )
 
-      (when (<= (+ 1 skipAmount fromValue) toValue)  
+      (when (< (+ 1 skipAmount fromValue) toValue)  
         (iterate-all-combinations (+ 1 fromValue) toValue skipAmount newSet newName))  
     )
 
-    (when (<= (+ 1 skipAmount fromValue) toValue)        
+    (when (< (+ 1 skipAmount fromValue) toValue)        
       (iterate-all-combinations fromValue toValue (+ 1 skipAmount) currentSet currentName))   
   )
   
-
-  (iterate-all-combinations 1 layersCount 0 '() "layer")  
+  (iterate-all-combinations 0 layersCount 0 '() "layer"))
 )
 
 (script-fu-register "script-fu-combinatorix-merge"
@@ -58,5 +66,5 @@
 )
 
 (script-fu-menu-register "script-fu-combinatorix-merge"
-                         "<Image>/Tools/CombinatorixMerge")
+                         "<Image>/Tools/")
 
