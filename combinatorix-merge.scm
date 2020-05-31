f776215f587e7c659cd93c4610e83e604dc00471
Oleksandr Novak <novak.as@i.ua>
2020-05-31 13:43:08 +0300

    set correct filename for every new image

--- combinatorix-merge.scm
+++ combinatorix-merge.scm
@@ -7,53 +7,70 @@
   )
 )
 
+(define (join-strings-with-delimiter delimiter list)
+  (let loop ((current "") (values list))
+    (if (null? values)
+        current
+        (loop (string-append current delimiter (car values)) (cdr values)))
+    )
+)
+
 (define (script-fu-combinatorix-merge)
   (let* (
     (imageWidth 50)
     (imageHeight 50)
     (originalImage (aref (cadr (gimp-image-list)) 0))
     (layersCount (car (gimp-image-get-layers originalImage)))
+    (imageNamePrefix "image")
+    (imageNameLayerSeparator "_") 
   )
 
   (define (get-layer-number image sequenceNumber)
     (aref (cadr (gimp-image-get-layers image)) sequenceNumber)
   )
 
-  (define (iterate-all-combinations fromValue toValue skipAmount currentSet currentName)
+  (define (get-original-layer-name layerNumber)
+    (car (gimp-item-get-name layerNumber))
+  )
 
-    (let (
-        (newSet (append (list (+ fromValue skipAmount)) currentSet))
-        (newName (string-append currentName "-" (number->string (+ fromValue skipAmount))))
-      )    
-      
-        (let (
-          (newImage (car (gimp-image-new imageWidth imageHeight RGB)))
-        ) 
-          (for-each newSet (lambda (layerSequenceNumber)
-                        
-            (gimp-image-insert-layer 
-              newImage
-              (head (gimp-layer-new newImage imageWidth imageHeight RGBA-IMAGE (number->string layerSequenceNumber) 100 LAYER-MODE-NORMAL))
-              0 0)
+  (define (iterate-all-combinations fromValue toValue skipAmount currentSet)
 
-            (gimp-image-set-active-layer originalImage (get-layer-number originalImage layerSequenceNumber))
+    (let* (
+      (newImage (car (gimp-image-new imageWidth imageHeight RGB)))
+      (newSet (append (list (get-layer-number originalImage (+ fromValue skipAmount))) currentSet))
+    ) 
+      (for-each newSet (lambda (layerId)
+                    
+        (gimp-image-insert-layer 
+          newImage
+          (head (gimp-layer-new newImage imageWidth imageHeight RGBA-IMAGE 
+                  (car (gimp-item-get-name layerId))
+                  100 LAYER-MODE-NORMAL))
+          0 0)
 
-            (gimp-edit-copy (car (gimp-image-get-active-layer originalImage)))
-            (gimp-floating-sel-anchor (car (gimp-edit-paste (car (gimp-image-get-active-layer newImage)) TRUE)))
-          ))                    
+        (gimp-image-set-active-layer originalImage layerId)
 
-          (gimp-display-new newImage)
+        (gimp-edit-copy (car (gimp-image-get-active-layer originalImage)))
+        (gimp-floating-sel-anchor (car (gimp-edit-paste (car (gimp-image-get-active-layer newImage)) TRUE)))
+      ))                    
+
+      (gimp-image-set-filename newImage 
+        (string-append imageNamePrefix 
+          (join-strings-with-delimiter imageNameLayerSeparator (map get-original-layer-name newSet)) 
         )
+      )
+
+      (gimp-display-new newImage)
 
       (when (< (+ 1 skipAmount fromValue) toValue)  
-        (iterate-all-combinations (+ 1 fromValue) toValue skipAmount newSet newName))  
+        (iterate-all-combinations (+ 1 fromValue) toValue skipAmount newSet))            
     )
 
     (when (< (+ 1 skipAmount fromValue) toValue)        
-      (iterate-all-combinations fromValue toValue (+ 1 skipAmount) currentSet currentName))   
+      (iterate-all-combinations fromValue toValue (+ 1 skipAmount) currentSet))   
   )
   
-  (iterate-all-combinations 0 layersCount 0 '() "layer"))
+  (iterate-all-combinations 0 layersCount 0 '()))
 )
 
 (script-fu-register "script-fu-combinatorix-merge"
